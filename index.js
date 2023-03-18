449f6b0e5b6937aa10af179a3b05bd43a9a807a0
dristosun <107073369+dristosun@users.noreply.github.com>
2023-03-18 19:43:24 +0200

    Finished project

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-const STANDARD_NUMBER = 0;
-
-let currentOperation = String(STANDARD_NUMBER);
-let currentOperator = '';
-let nr1 = STANDARD_NUMBER;
-let nr2 = STANDARD_NUMBER;
-
 const display = document.querySelector('.display');
-const preview = document.querySelector('.preview');
-const numbers = [document.querySelector('.one'), document.querySelector('.two'), document.querySelector('.three'), document.querySelector('.four'),document.querySelector('.five'),document.querySelector('.six'), document.querySelector('.seven'), document.querySelector('.eight'), document.querySelector('.nine')];
-const operators = [document.querySelector('.divide'), document.querySelector('.times'), document.querySelector('.minus'), document.querySelector('.plus'), document.querySelector('.equal')];
-const buttons = [numbers, operators];
+const previous = document.querySelector('.previousNumber');
+const current = document.querySelector('.currentNumber');
+const numbers = document.querySelectorAll('.digit');
+const operators = document.querySelectorAll('.operand');
+const equal = document.querySelector('.equal');
+const clear = document.querySelector('.clearAll');
+const changeSign = document.querySelector('.changeSign');
 
 class Calculator {
     constructor(previousNumber, currentNumber) {
@@ -19,23 +15,142 @@ class Calculator {
     }
 
     clear() {
-        this.previousNumber = '';
         this.currentNumber = '';
+        this.previousNumber = '';
         this.operation = undefined;
     }
+
+    changeSign() {
+        this.currentNumber = 0 - this.currentNumber;
+    }
+
+    appendNumber(nr) {
+        this.currentNumber = this.currentNumber.toString();
+            if (this.currentNumber.includes('.') && nr === ".") {
+                return;
+            }
+            if (this.currentNumber.length > 15) {
+                return;
+            }
+        this.currentNumber = this.currentNumber + nr.toString();
+    }
+
+    updateDisplay() {
+        previous.textContent = '';
+        current.textContent = this.currentNumber;
+        if (this.operation != undefined)
+            previous.textContent = `${this.previousNumber} ${this.operation} `
+    }
+
+    operate(operator) {
+        if (this.currentNumber === '') return;
+        if (this.previousNumber !== '')
+            this.equal();
+        this.operation = operator;
+        this.previousNumber = this.currentNumber;
+        this.currentNumber = '';
+    }
+
+    equal() {
+        if (this.currentNumber === '')
+            return;
+        const prev = parseFloat(this.previousNumber);
+        const current = parseFloat(this.currentNumber);
+        switch(this.operation) {
+            case "+":
+                this.currentNumber = prev + current;
+                break;
+            case "-":
+                this.currentNumber = prev - current;
+                break;
+            case "÷":
+                this.currentNumber = prev / current;
+                break;
+            case "×":
+                this.currentNumber = prev * current;
+                break;
+        }
+            this.operation = undefined;
+            this.previousNumber = '';
+            previous.textContent = '';
+    }
+
+    delete() {
+        if (this.currentNumber === '') {
+            this.operation = undefined;
+            this.currentNumber = this.previousNumber;
+            this.previousNumber = '';
+        }
+        else {
+            console.log(this.currentNumber);
+            this.currentNumber = String(this.currentNumber);
+            const length = this.currentNumber.length; 
+            this.currentNumber = this.currentNumber.slice(0, length-1)
+        }
+    }
 }
-const  calculator = new Calculator(nr1, nr2)
+
+const  calculator = new Calculator('', '');
+
 numbers.forEach(number => { 
     number.onclick = function() {
+        calculator.appendNumber(number.textContent);
         calculator.updateDisplay();
     }
 })
 
 operators.forEach(operator => { 
     operator.onclick = function() {
-        calculator.operate();
+        calculator.operate(operator.textContent);
         calculator.updateDisplay();
     }
 })
+equal.onclick = function() {
+    calculator.equal();
+    calculator.updateDisplay();
+}
 
-cr cr
+clear.onclick = function() {
+    calculator.clear();
+    calculator.updateDisplay();
+}
+
+changeSign.onclick = function() {
+    calculator.changeSign();
+    calculator.updateDisplay();
+}
+
+function clickButton(button, name) {
+    button.click();
+    button.classList.add(name);
+    setTimeout(function() {
+        button.classList.remove(name);
+    }, 100)
+}
+
+document.addEventListener('keydown', e => {
+    if (e.key >= 0 && e.key <=9 || e.key === '.') {
+        numbers.forEach(number => {
+            if (number.textContent === e.key) {
+                clickButton(number, 'digitHover');
+            }
+        })
+    }
+    else if (e.keyCode >= 106 && e.keyCode <= 111) {
+        operators.forEach(operator => {
+            if ((e.keyCode === 106 && operator.textContent === '×') || (operator.textContent === e.key) || (e.keyCode === 111 && operator.textContent === '÷')) {
+                clickButton(operator, 'operandHover'); 
+            }
+        })
+    }
+    else if (e.key === '=') {
+        clickButton(equal, 'equalHover');
+    }
+
+    else if (e.keyCode === 8) {
+        calculator.delete();
+        calculator.updateDisplay();
+    }
+    else if (e.keyCode === 46)
+        clickButton(clear, 'clearHover');
+})
